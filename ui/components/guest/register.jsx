0a1f3794d98037d6d4bfa906ef9cc1b2046df120
Draku <vladimir.ibazeta@gmail.com>
2018-09-15 19:21:05 -0500

    Rewrite request method, implement /account/exists endpoint, validate Register form and actually do registration now

--- ui/components/guest/register.jsx
+++ ui/components/guest/register.jsx
@@ -1,25 +1,58 @@
 import map from 'lodash/map'
+import clone from 'lodash/clone'
+import find from 'lodash/find'
 import findIndex from 'lodash/findIndex'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Dropdown, Header, Form, Divider, Button, Icon } from 'semantic-ui-react'
+import { Container, Dropdown, Header, Form, Message, Divider, Button, Icon } from 'semantic-ui-react'
+import { validate } from '../../helpers'
 import { ID_TYPES, SEXES } from '../../data'
-import { School } from '../../../lib'
+import { Account, School, Person } from '../../../lib'
+
+const STATE = {
+    // Data
+    idType: '',
+    id: '',
+    firstName: '',
+    lastName: '',
+    age: '',
+    sex: '',
+    schoolId: '',
+    email: '',
+    address: '',
+    phone: '',
+    password: '',
+    repeatPassword: '',
+
+    // Validation flags
+    idTypeIsValid: null,
+    idIsValid: null,
+    firstNameIsValid: null,
+    lastNameIsValid: null,
+    ageIsValid: null,
+    sexIsValid: null,
+    schoolIdIsValid: null,
+    emailIsValid: null,
+    addressIsValid: null,
+    phoneIsValid: null,
+    passwordIsValid: null,
+    repeatPasswordIsValid: null,
+    idIsNotAvailable: null,
+    emailIsNotAvailable: null,
+
+    // Control
+    schools: [],
+    fetchedSchools: false,
+    idLabel: '...',
+    hasSelectedIdType: false,
+    canSubmit: false,
+    submitting: false,
+    errored: false,
+    finished: null
+}
 
 export default class Register extends Component {
-    state = {
-        // Data
-        idType: '',
-        id: '',
-        sex: '',
-        schoolId: '',
-
-        // Control
-        schools: [],
-        fetchedSchools: false,
-        idLabel: '...',
-        hasSelectedIdType: false
-    }
+    state = clone(STATE)
 
     componentDidMount() {
         School.all().then(schools => {
@@ -38,12 +71,69 @@ export default class Register extends Component {
         })
     }
 
-    handleChange = ({ name, value }) => {
-        console.log(name, value)
+    validateInput = (name, value) => validate(name, value, name === 'repeatPassword' ? this.state.password : (
+        name === 'id' ? this.state.idType : null
+    ))
+
+    isSubmittable = () => {
+        const {
+            idTypeIsValid,
+            idIsValid,
+            firstNameIsValid,
+            lastNameIsValid,
+            ageIsValid,
+            sexIsValid,
+            schoolIdIsValid,
+            emailIsValid,
+            addressIsValid,
+            phoneIsValid,
+            passwordIsValid,
+            repeatPasswordIsValid
+        } = this.state
+
+        return (
+            idTypeIsValid &&
+            idIsValid &&
+            firstNameIsValid &&
+            lastNameIsValid &&
+            ageIsValid &&
+            sexIsValid &&
+            schoolIdIsValid &&
+            emailIsValid &&
+            addressIsValid &&
+            phoneIsValid &&
+            passwordIsValid &&
+            repeatPasswordIsValid
+        )
+    }
+
+    handleChange = ({ target }) => {
+        const { name, value } = target
+        const nameIsValid = name + 'IsValid'
+        const isValid = this.validateInput(name, value)
+
+        if (!isValid) {
+            this.setState({
+                [name]: value,
+                [nameIsValid]: isValid,
+                canSubmit: false
+            })
+
+            return
+        }
+        
+        this.state[nameIsValid] = isValid
+
+        this.setState({
+            [name]: value,
+            [nameIsValid]: isValid,
+            canSubmit: this.isSubmittable()
+        })
     }
 
     handleSelectChange = (_, d) => {
         const { name, value } = d
+        const isValid = this.validateInput(name, value)
 
         if (name === 'idType') {
             const { text } = ID_TYPES[findIndex(ID_TYPES, idType => idType.value === value)]
@@ -51,27 +141,178 @@ export default class Register extends Component {
             this.setState({
                 [name]: value,
                 idLabel: text,
-                hasSelectedIdType: true
+                idTypeIsValid: isValid,
+                hasSelectedIdType: true,
+                canSubmit: false,
+
+                id: '',
+                idIsValid: null
             })
 
             return
         }
 
+        const nameIsValid = name + 'IsValid'
+
+        if (!isValid) {
+            this.setState({
+                [name]: value,
+                [nameIsValid]: false,
+                canSubmit: false
+            })
+
+            return
+        }
+
+        this.state[nameIsValid] = true
+
         this.setState({
-            [name]: value
+            [name]: value,
+            [nameIsValid]: true,
+            canSubmit: this.isSubmittable()
         })
     }
 
     handleSubmit = () => {
-        console.log('submit')
+        const { submitting } = this.state
+
+        if (submitting) {
+            return
+        }
+
+        this.setState({
+            submitting: true
+        })
+
+        const { id, email } = this.state
+
+        Promise.all([Person.exists({ id: id }), Person.exists({ email: email })]).then(([ idIsUnavailable, emailIsUnavailable ]) => {
+            if (idIsUnavailable || emailIsUnavailable) {
+                this.setState({
+                    idIsNotAvailable: idIsUnavailable,
+                    emailIsNotAvailable: emailIsUnavailable,
+                    errored: true,
+                    finished: true,
+                    submitting: false
+                })
+
+                return
+            }
+
+            const {
+                id,
+                idType,
+                firstName,
+                lastName,
+                age,
+                sex,
+                schoolId,
+                email,
+                address,
+                phone,
+                password
+            } = this.state
+
+            Account.new({
+                id: id,
+                idType: idType,
+                firstName: firstName,
+                lastName: lastName,
+                age: age,
+                sex: sex,
+                schoolId: schoolId,
+                email: email,
+                address: address,
+                phone: phone,
+                password: password
+            }).then(() => {
+                this.setState({
+                    id: '',
+                    idType: '',
+                    firstName: '',
+                    lastName: '',
+                    age: '',
+                    sex: '',
+                    schoolId: '',
+                    email: '',
+                    address: '',
+                    phone: '',
+                    password: '',
+                    repeatPassword: '',
+                    idIsNotAvailable: false,
+                    emailIsNotAvailable: false,
+                    errored: false,
+                    finished: true,
+                    submitting: false,
+                    canSubmit: false
+                })
+            }).catch(() => {
+                this.setState({
+                    idIsNotAvailable: null,
+                    emailIsNotAvailable: null,
+                    errored: true,
+                    finished: true,
+                    submitting: false
+                })
+            })
+        }).catch(() => {
+            this.setState({
+                idIsNotAvailable: null,
+                emailIsNotAvailable: null,
+                errored: true,
+                finished: true,
+                submitting: false
+            })
+        });
     }
 
     render() {
         // Data
-        const { id, idType, sex } = this.state
+        const {
+            id,
+            idType,
+            firstName,
+            lastName,
+            age,
+            sex,
+            schoolId,
+            email,
+            address,
+            phone,
+            password,
+            repeatPassword
+        } = this.state
+
+        const {
+            idTypeIsValid,
+            idIsValid,
+            firstNameIsValid,
+            lastNameIsValid,
+            ageIsValid,
+            sexIsValid,
+            schoolIdIsValid,
+            emailIsValid,
+            addressIsValid,
+            phoneIsValid,
+            passwordIsValid,
+            repeatPasswordIsValid,
+
+            // Availability checks
+            idIsNotAvailable,
+            emailIsNotAvailable
+        } = this.state
 
         // Control
-        const { schools, fetchedSchools, idLabel, hasSelectedIdType } = this.state
+        const {
+            schools,
+            fetchedSchools,
+            idLabel,
+            hasSelectedIdType,
+            submitting,
+            canSubmit,
+            errored,
+            finished
+        } = this.state
 
         return (
             <React.Fragment>
@@ -80,11 +321,11 @@ export default class Register extends Component {
                 <Form onSubmit={this.handleSubmit}>
                     <Form.Field required>
                         <label>Tipo de documento</label>
-                        <Form.Select name='idType' value={idType} fluid options={ID_TYPES} placeholder='Tipo de documento' onChange={this.handleSelectChange} />
+                        <Form.Select disabled={submitting} error={idTypeIsValid === false} name='idType' value={idType} fluid options={ID_TYPES} placeholder='Tipo de documento' onChange={this.handleSelectChange} />
                     </Form.Field>
                     <Form.Field required>
                         <label>{idLabel}</label>
-                        <Form.Input disabled={!hasSelectedIdType} iconPosition='left' placeholder={idLabel}>
+                        <Form.Input disabled={submitting} error={idIsValid === false} disabled={!hasSelectedIdType} iconPosition='left' placeholder={idLabel}>
                             <Icon name='user circle' />
                             <input name='id' value={id} onChange={this.handleChange} />
                         </Form.Input>
@@ -94,55 +335,55 @@ export default class Register extends Component {
 
                     <Form.Field required>
                         <label>Nombres completos</label>
-                        <Form.Input iconPosition='left' placeholder='Nombres completos'>
+                        <Form.Input disabled={submitting} error={firstNameIsValid === false} iconPosition='left' placeholder='Nombres completos'>
                             <Icon name='font' />
-                            <input name='firstName' onChange={this.handleChange} />
+                            <input name='firstName' value={firstName} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
                     <Form.Field required>
                         <label>Apellidos completos</label>
-                        <Form.Input iconPosition='left' placeholder='Apellidos completos'>
+                        <Form.Input disabled={submitting} error={lastNameIsValid === false} iconPosition='left' placeholder='Apellidos completos'>
                             <Icon name='font' />
-                            <input name='lastName' onChange={this.handleChange} />
+                            <input name='lastName' value={lastName} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
                     <Form.Field required>
                         <label>Edad</label>
-                        <Form.Input iconPosition='left' placeholder='Edad'>
+                        <Form.Input disabled={submitting} error={ageIsValid === false} iconPosition='left' placeholder='Edad'>
                             <Icon name='birthday cake' />
-                            <input name='age' onChange={this.handleChange} />
+                            <input name='age' value={age} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
                     <Form.Field required>
                         <label>Sexo</label>
-                        <Form.Select name='sex' value={sex} fluid labeled={false} options={SEXES} placeholder='Sexo' onChange={this.handleSelectChange} />
+                        <Form.Select disabled={submitting} error={sexIsValid === false} name='sex' value={sex} fluid labeled={false} options={SEXES} placeholder='Sexo' onChange={this.handleSelectChange} />
                     </Form.Field>
 
                     <Divider section />
                     <Form.Field required>
                         <label>Escuela</label>
-                        <Dropdown name='schoolId' placeholder='Escuela' fluid search selection options={schools} loading={!fetchedSchools} onChange={this.handleSelectChange} />
+                        <Dropdown disabled={submitting} error={schoolIdIsValid === false} name='schoolId' value={schoolId} placeholder='Escuela' fluid search selection options={schools} loading={!fetchedSchools} onChange={this.handleSelectChange} />
                     </Form.Field>
 
                     <Form.Field required>
                         <label>Correo electrónico</label>
-                        <Form.Input iconPosition='left' placeholder='Correo electrónico'>
+                        <Form.Input disabled={submitting} error={emailIsValid === false} iconPosition='left' placeholder='Correo electrónico'>
                             <Icon name='at' />
-                            <input name='email' onChange={this.handleChange} />
+                            <input name='email' value={email} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
                     <Form.Field required>
                         <label>Dirección</label>
-                        <Form.Input iconPosition='left' placeholder='Dirección'>
+                        <Form.Input disabled={submitting} error={addressIsValid === false} iconPosition='left' placeholder='Dirección'>
                             <Icon name='address card' />
-                            <input name='address' onChange={this.handleChange} />
+                            <input name='address' value={address} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
                     <Form.Field required>
                         <label>Teléfono</label>
-                        <Form.Input iconPosition='left' placeholder='Teléfono'>
+                        <Form.Input disabled={submitting} error={phoneIsValid === false} iconPosition='left' placeholder='Teléfono'>
                             <Icon name='phone' />
-                            <input name='phone' onChange={this.handleChange} />
+                            <input name='phone' value={phone} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
 
@@ -150,26 +391,36 @@ export default class Register extends Component {
 
                     <Form.Field required>
                         <label>Contraseña</label>
-                        <Form.Input iconPosition='left' type='password' placeholder='Contraseña'>
+                        <Form.Input disabled={submitting} error={passwordIsValid === false} iconPosition='left' type='password' placeholder='Contraseña'>
                             <Icon name='key' />
-                            <input name='password' onChange={this.handleChange} />
+                            <input name='password' value={password} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
                     <Form.Field required>
                         <label>Repetir contraseña</label>
-                        <Form.Input iconPosition='left' type='password' placeholder='Repetir contraseña'>
+                        <Form.Input disabled={submitting} error={repeatPasswordIsValid === false} iconPosition='left' type='password' placeholder='Repetir contraseña'>
                             <Icon name='key' />
-                            <input name='repeatPassword' onChange={this.handleChange} />
+                            <input name='repeatPassword' value={repeatPassword} onChange={this.handleChange} />
                         </Form.Input>
                     </Form.Field>
 
                     <Container fluid textAlign='center'>
                         <Button.Group className='centered'>
-                            <Button as={Link} to='/'>Cancelar</Button>
+                            <Button as={Link} to='/' disabled={submitting}>Cancelar</Button>
                             <Button.Or text='o' />
-                            <Button positive type='submit'>Registrar</Button>
+                            <Button positive type='submit' loading={submitting} disabled={!canSubmit}>Registrar</Button>
                         </Button.Group>
                     </Container>
+
+                    <Message error={errored} success={!errored} visible={finished !== null}>
+                        <Message.Header>{errored ? 'Oh no! Ha sucedido algo' : 'Felicitaciones!'}</Message.Header>
+                        {((idIsNotAvailable === true) || emailIsNotAvailable === true) ? (
+                            <Message.List>
+                                {idIsNotAvailable ? <Message.Item>{find(ID_TYPES, ({ value }) => value === idType).text} ya se encuentra en uso</Message.Item> : null}
+                                {emailIsNotAvailable ? <Message.Item>Correo electrónico ya se encuentra en uso</Message.Item> : null}
+                            </Message.List>
+                        ) : (errored ? <p>Algo ha salido mal, por favor inténtalo de nuevo más tarde</p> : <p>Te has registrado exitosamente</p>)}
+                    </Message>
                 </Form>
             </React.Fragment>
         )
--- ui/helpers/validation.js
+++ ui/helpers/validation.js
@@ -3,16 +3,69 @@ import toLower from 'lodash/toLower'
 import isEmail from 'validator/lib/isEmail'
 import isEmpty from 'validator/lib/isEmpty'
 import isLength from 'validator/lib/isLength'
+import isNumeric from 'validator/lib/isNumeric'
 import { default as isEqual }  from 'validator/lib/equals'
 
+const MAX_ID_TYPE = 3
+const MAX_AGE = 150
+const MAX_SEX = 3
+
 const validator = {
+    id: function(value, type = null) {
+        let isValid = !isEmpty(value)
+
+        if (!isValid) {
+            return false
+        }
+
+        switch(type) {
+            case 1: // DNI
+                return isLength(value, { min: 7, max: 8 }) && isNumeric(value)
+            case 2: // Carné de extranjería
+                return isLength(value, { min: 7, max: 9 }) && isNumeric(value)
+            case 3: // Pasaporte
+                return isLength(value, { min: 9, max: 9 }) && isNumeric(value)
+        }
+
+        return false
+    },
+    idtype: function(value) {
+        if ('number' === typeof value) value = value.toString()
+
+        return !isEmail(value) && isLength(value, { min: 1, max: 1 }) && isNumeric(value) && +value <= MAX_ID_TYPE
+    },
+    firstname: function(value) {
+        return !isEmail(value) && isLength(value, { min: 1, max: 128 })
+    },
+    lastname: function(value) {
+        return !isEmail(value) && isLength(value, { min: 1, max: 128 })
+    },
+    age: function (value) {
+        return !isEmail(value) && isLength(value, { min: 1, max: 3 }) && isNumeric(value) && +value <= MAX_AGE
+    },
+    sex: function (value) {
+        if ('number' === typeof value) value = value.toString()
+
+        return !isEmail(value) && isLength(value, { min: 1, max: 1 }) && isNumeric(value) && +value <= MAX_SEX
+    },
+    schoolid: function (value) {
+        if ('number' === typeof value) value = value.toString()
+
+        return !isEmail(value) && isNumeric(value)
+    },
     email: function(value) {
         return !isEmpty(value) && isLength(value, {min: 12, max: 64}) && isEmail(value)
+    },
+    address: function (value) {
+        return !isEmpty(value) && isLength(value, {min: 12, max: 255 })
+    },
+    phone: function (value) {
+        return !isEmpty(value) && isLength(value, {min: 9, max: 32 })
     },
     password: function(value) {
         return !isEmpty(value) && isLength(value, {min: 6, max: 24})
     },
-    repeatPassword: function(value, passwordValue) {
+    repeatpassword: function(value, passwordValue) {
         return !isEmpty(value) && isLength(value, {min: 6, max: 24}) && isEqual(value, passwordValue)
     }
 }
@@ -22,17 +75,18 @@ const validator = {
  * 
  * @param {String} key
  * @param {String} value
+ * @param {String} [repeat=]
  * 
  * @returns {boolean} True if value is okay and false if not
  */
-export default function validate(key, value) {
+export default function validate(key, value, repeat = null) {
     const lowercaseKey = toLower(key)
 
     if (!has(validator, lowercaseKey)) {
         return true
     }
 
-    return validator[lowercaseKey](value)
+    return validator[lowercaseKey](value, repeat)
 }
 
 /**
